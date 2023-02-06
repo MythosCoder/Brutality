27d23f46291ea950ad82be951854ac6501c50d19
root <root@DeviceK>
2023-02-06 07:38:46 -0500

    modo de Fuzzing agregado, Falta mucho codigo por optimizar

--- brutal-log.py
+++ brutal-log.py
@@ -14,14 +14,11 @@ print(result)
 BrutusDescription = "Brutal-log is a tool made for brute forcing web logins"
 parser = argparse.ArgumentParser(description = BrutusDescription, prog = "Brutal-Log", add_help = False)
 parser.add_argument('-h', "--help", help = "displays help", action = "help")
-parser.add_argument('-m', "--method", required=True, choices = ("GET", "POST"), help = "Specifies METHOD used be it POST or GET")
+parser.add_argument('-f', "--fuzz", action = "store_true", help = "directory enumeration mode")
+parser.add_argument('-m', "--method", required=True, choices = ("get", "post"), help = "Specifies METHOD used be it POST or GET")
 parser.add_argument('-w', "--wordlist", action = "store", required=True, help = "Specifies the WORDLIST used for the attack")
 parser.add_argument("-u", '--url', required = True, help = "Specifies the target URL")
 args = parser.parse_args()
-
-#remainder of made code that detects if website is using https or http
-def DetectHttps():
-	pass
 
 #wordlist
 lines = []
@@ -37,6 +34,7 @@ if(os.path.isfile(args.wordlist)):
 else:
 	print(colored("ERROR", "red"),"\nplease check file path -->", "\"" + args.wordlist + "\"")
 	sys.exit(0)
+
 
 #incase of POST request
 def POST_Req():
@@ -63,12 +61,25 @@ def statusCode(code):
 	elif(code.status_code >= 300 and code.status_code < 400):
 		return colored(code.status_code, "orange")
 
+
+def FuzzingMode():
+	try:
+		print("using fuzzing mode")
+		for i in lines:
+			reqGET = requests.get(args.url + "/" + i)
+			print(colored(args.url + "/" + i, "blue"), "<-------> status code:", statusCode(reqGET))
+	except requests.exceptions.ConnectionError:
+		print(colored("Connection ERROR", "red"))
+	else:
+		print("error")
+
 #incase of GET request
 def GET_Req():
 	try:
 		print("Sending GET Request")
 		for i in lines:
-			reqGET = requests.get(args.url + i)
+			reqGET = requests.get(args.url)
+			print(args.url + i)
 			print(colored(args.url, "blue"), "<---------> status code:", statusCode(reqGET))
 	except requests.exceptions.MissingSchema:
 		print(colored("Error", "red"),"\nMissing Schema, did you mean?: http(s)://" + args.url)
@@ -78,11 +89,13 @@ def GET_Req():
 		print("Connection Error")
 
 def run():
-#	if(os.getuid() != 0):
-#		print("You must have privileges to use Brutus!")
-#		sys.exit(0)
-	if(args.method == "POST"):
+	if(os.getuid() != 0):
+		print("You must have privileges to use Brutus!")
+		sys.exit(0)
+	if(args.fuzz == True):
+		FuzzingMode()
+	if(args.method == "post"):
 		POST_Req()
-	elif(args.method == "GET"):
+	elif(args.method == "get"):
 		GET_Req()
 run()
